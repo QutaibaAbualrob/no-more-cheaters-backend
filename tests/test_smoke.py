81d84ead53303e081f8d2c20a8ac87b28732e1c7
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-04-12 18:02:37 +0300

    restart 0.1

--- tests/test_smoke.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from django.urls import reverse
-
-
-@pytest.mark.django_db
-def test_token_obtain_requires_user(api_client):
-    url = reverse("token_obtain_pair")
-    r = api_client.post(url, {"username": "nope", "password": "bad"}, format="json")
-    assert r.status_code == 401
