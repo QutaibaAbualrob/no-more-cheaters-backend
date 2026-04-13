b53b600f7b99d0c3fdaa641658c3f80055f1732e
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-04-13 18:17:57 +0300

    0.1 added restframework

---
+++ nomorecheaters/nomorecheaters/views.py
@@ -0,0 +1,9 @@
+# from django.contrib import admin
+# from django.contrib.auth import authenticate, login
+# from django.contrib.auth.forms import UserCreationForm
+# from django.shortcuts import render, redirect
+
+
+# class Register:
+#     pass
+    
