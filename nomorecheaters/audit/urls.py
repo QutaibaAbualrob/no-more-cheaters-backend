fbcc0db9d215810c9f4b0cc55f99623004a8748b
Haitham-Khalil <khalilhaitham560@gmail.com>
2026-05-01 17:05:02 +0300

    removed sbehat changes #dc50f09

--- nomorecheaters/audit/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import SystemLogListView, SystemMetricsView
-
-urlpatterns = [
-    path("system/logs/", SystemLogListView.as_view(), name="system_logs"),
-    path("system/metrics/", SystemMetricsView.as_view(), name="system_metrics"),
-]
-
