c2ca7f89797e0ae23bb94f739608d3453089e4e7
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-06-15 22:16:22 +0300

    updated email templates

---
+++ nomorecheaters/apis/migrations/0007_update_site_domain.py
@@ -0,0 +1,28 @@
+# Generated migration to set the production Site domain.
+from django.db import migrations
+
+
+def update_site_domain(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    Site.objects.filter(id=1).update(
+        domain="nomorecheater.online",
+        name="No More Cheaters",
+    )
+
+
+def reverse_site_domain(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    Site.objects.filter(id=1).update(
+        domain="example.com",
+        name="example.com",
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(update_site_domain, reverse_site_domain),
+    ]
